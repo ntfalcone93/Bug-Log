7a95de9f7ea8337559ddb863b52ca8a803728ed5
Nathan Falcone <ntfalcone93@gmail.com>
2018-10-10 20:02:42 -0600

    Simple setup for BugDetailVC

--- Bug Log/View Controllers/BugDetailViewController.swift
+++ Bug Log/View Controllers/BugDetailViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class BugDetailViewController: UIViewController {
 
+    // MARK: @IBOutlets
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextField: UITextView!
+
+    // MARK: Lifecycle methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: @IBActions
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        // TODO: Save bug
     }
-    */
-
 }

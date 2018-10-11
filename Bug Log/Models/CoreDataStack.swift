06e993dd125fcfb948803fe0e67f39a8a3033e0d
Nathan Falcone <ntfalcone93@gmail.com>
2018-10-11 17:15:53 -0600

    Added CoreDataStack, removed data model

---
+++ Bug Log/Models/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Frank Martin Jr on 10/11/18.
+//  Copyright © 2018 Travis Chapman. All rights reserved.
+//
+//  Using the CoreDataStack from Journal for familiarity
+
+import Foundation
+import CoreData
+
+struct CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var managedObjectContext: NSManagedObjectContext { return container.viewContext }
+}

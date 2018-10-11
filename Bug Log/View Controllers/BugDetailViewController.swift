2a77892fce6bfcf0398fd275239bddabab6d21eb
Nathan Falcone <ntfalcone93@gmail.com>
2018-10-10 19:15:57 -0600

    Added view controller files

---
+++ Bug Log/View Controllers/BugDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BugDetailViewController.swift
+//  Bug Log
+//
+//  Created by Nathan Falcone on 10/10/18.
+//  Copyright © 2018 Nathan Falcone Development. All rights reserved.
+//
+
+import UIKit
+
+class BugDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

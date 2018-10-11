86641e8cd6b0515ac7bd48153eab7956cbc14978
Nathan Falcone <ntfalcone93@gmail.com>
2018-10-11 17:44:31 -0600

    Added BugExtension

---
+++ Bug Log/Models/BugExtension.swift
@@ -0,0 +1,27 @@
+//
+//  BugExtension.swift
+//  Bug Log
+//
+//  Created by Nathan Falcone on 10/11/18.
+//  Copyright © 2018 Nathan Falcone Development. All rights reserved.
+//
+
+import Foundation
+
+extension Bug {
+
+    convenience init(title: String,
+                     log: String,
+                     severity: Int,
+                     causation: String,
+                     steps: String) {
+
+        self.init(context: CoreDataStack.managedObjectContext)
+        
+        self.title = title
+        self.log = log
+        self.severity = Int16(severity)
+        self.causation = causation
+        self.steps = steps
+    }
+}
